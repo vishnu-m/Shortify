1a62980fb1eb954cb335a441ef6808f22d3266f2
Sreeraj <sreerajavk123@gmail.com>
2018-05-22 09:57:31 +0530

    editprofile modified

---
+++ static/js/editprofile.js
@@ -0,0 +1,40 @@
+
+// validating email  ( whether already exists )
+
+$('#edit_profile').on('submit',function(event){
+
+    event.preventDefault();
+
+
+    var first_name = $('#first_name').val();
+    var last_name = $('#last_name').val();
+    var email = $('#email').val();
+
+
+
+
+    $.post('/editprofile/',{
+        first_name:first_name,
+        last_name:last_name,
+        email:email,
+
+
+    })
+    .done(function(response){
+        // request successfull
+        if ( response.status == '200'){
+            // redirect to home page
+            window.location = "/";
+        }
+        show_warning(response.text);
+    })
+    .fail(function(response){
+        // request failed
+        show_warning('Something went wrong')
+    });
+});
+
+function show_warning(warning){
+    $('#warning_txt').html(warning);
+    $('#warning_txt').css('color','#f00');
+}
